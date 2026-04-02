c12a0cce0dc0db10faa1300b6291fa20c699c03d
kk3494452-ship-it <kk3494452@gmail.com>
2026-04-03 00:20:43 +0300

    Update main.py
    تطبيق

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from kivy.uix.button import Button
 
 class MyApp(App):
     def build(self):
-        # هذا الزر سيظهر في وسط شاشة جوالك عند تشغيل التطبيق
-        return Button(text='مرحباً بك في تطبيقي الأول!\nاضغط هنا', 
+        return Button(text='Welcome to my School App', 
                       background_color=(0, 0.5, 1, 1),
-                      font_size='20sp')
+                      font_size='25sp')
 
 if __name__ == '__main__':
     MyApp().run()
-  
+    
